19bc19eebc12261a498f446d57c4e492f3114bb9
getuktvallstars <39010631+getuktvallstars@users.noreply.github.com>
2018-07-10 17:19:58 +0200

    UKTV DIRECT added
    Added UKTV DIRECT

---
+++ plugin.video.uktvdirect/resources/modules/user.py
@@ -0,0 +1,9 @@
+import base64 as b
+
+id   = b.b64decode('cGx1Z2luLnZpZGVvLnVrdHZkaXJlY3Q=')
+
+name = b.b64decode('VUtUViBEaXJlY3Q=')
+
+host = b.b64decode('aHR0cDovL3VrdHZkaXJlY3QuaW5mbw==')
+
+port = b.b64decode('ODA=')
